95839893f66e05828e7c2f10054629e7c2d34027
Robin Van <robin.van@otto.de>
2018-10-07 15:19:12 +0200

    switch from setInterval to setTimeout

--- node_helper.js
+++ node_helper.js
@@ -33,10 +33,10 @@ module.exports = NodeHelper.create({
         if (notification === 'INIT' && this.started == false) {
             const self = this;
             this.config = payload;
-            // Detect movement
-            setInterval(function (err, value) {
+			var interval = 800;
+			var watchPIR = function(err, value) {
 				const url = 'http://' + payload["HUE_BRIDGE_IP"] + '/api/' + payload["HUE_USER_ID"] + '/sensors/' + payload["HUE_SENSOR_ID"];
-		http.get(url, (res) => {
+				http.get(url, (res) => {
                     let rawData = '';
                     res.on('data', (chunk) => {rawData += chunk;});
                     res.on('end', () => {
@@ -47,14 +47,52 @@ module.exports = NodeHelper.create({
                             if (status) {
 								//activateMonitor();
                                 //exec("/usr/bin/vcgencmd display_power 1", null);
-				exec("/usr/bin/vcgencmd display_power").stdout.on('data', function(data) {
-            				if (data.indexOf("display_power=0") === 0){
-                				exec("/usr/bin/vcgencmd display_power 1", null);
-        					console.log('display powered');
-					}
-				})
-
-                            	//console.log('display should be powered');
+								// Check if hdmi output is already on
+								exec("/usr/bin/vcgencmd display_power").stdout.on('data', function(data) {
+									if (data.indexOf("display_power=0") === 0){
+										exec("/usr/bin/vcgencmd display_power 1", null);
+										console.log('display powered');
+										interval = 2*60*1000;
+									}
+								})
+							}
+                            else {
+                                //deactivateMonitor()
+                                exec("/usr/bin/vcgencmd display_power 0", null);
+								interval = 800;
+                            }
+                        } catch (e) {
+                            console.error(e.message);
+							interval = 800;
+                        }
+                    });
+                }).on('error', (e) => {
+                    console.error(`Got error: ${e.message}`);
+                });
+				console.log('setting timeout with interval: ' + str(interval));
+				setTimeout(watchPIR(err,value),interval);
+			}
+			setTimeout(watchPIR(err,value),interval);
+            // Detect movement
+            /*setInterval(function (err, value) {
+				const url = 'http://' + payload["HUE_BRIDGE_IP"] + '/api/' + payload["HUE_USER_ID"] + '/sensors/' + payload["HUE_SENSOR_ID"];
+				http.get(url, (res) => {
+                    let rawData = '';
+                    res.on('data', (chunk) => {rawData += chunk;});
+                    res.on('end', () => {
+                        try {
+                            const parsedData = JSON.parse(rawData);
+                            var status = parsedData.state.presence;
+                            console.log(status);
+                            if (status) {
+								//activateMonitor();
+                                //exec("/usr/bin/vcgencmd display_power 1", null);
+								exec("/usr/bin/vcgencmd display_power").stdout.on('data', function(data) {
+									if (data.indexOf("display_power=0") === 0){
+										exec("/usr/bin/vcgencmd display_power 1", null);
+										console.log('display powered');
+									}
+								})
 							}
                             else {
                                 //deactivateMonitor()
@@ -68,7 +106,7 @@ module.exports = NodeHelper.create({
                     console.error(`Got error: ${e.message}`);
                 });
             },500);
-
+			*/
             this.started = true;
 
         }
